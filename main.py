50729513c41001a3ad4b71297d9e3cea6ba3b498
Daniel Pelzel <pelzel.daniel@gmail.com>
2026-06-19 18:10:48 +0200

    in main Code für Karte hinzugefügt

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from nbclient.client import timestamp
-
+import pandas as pd
+import folium
+import xyzservices.providers as xyz
 from video_gps import *
 
-Koordinaten = []
+
+#---GPS Koordinaten aus Video auslesen---
 
 video = VideoGPS("/Volumes/Untitled/DCIM/100MEDIA/DJI_0079.SRT")
-
-#for i in timestamps:
-#    cord = video.get_gps_from_time(i)
-#    Koordinaten.append(cord["lon"],cord["lat"])
+koordinaten = []
 
 
-#single_cords = video.get_gps_from_time(timedelta(seconds=10))
-#Koordinaten.append(single_cords)
+#Testkoordinaten zum Testen aus Video, später Liste von Timestamps übergeben mit einer for Schleife
+koordinaten.append([video.get_gps_from_time(timedelta(seconds=10))["lat"],
+                    video.get_gps_from_time(timedelta(seconds=10))["lon"]])
+koordinaten.append([video.get_gps_from_time(timedelta(seconds=20))["lat"],
+                    video.get_gps_from_time(timedelta(seconds=20))["lon"]])
 
-cord = video.get_gps_from_time(timedelta(seconds=10))
-cord2= video.get_gps_from_time(timedelta(seconds=12))
-Koordinaten.append([cord["lat"],cord["lon"]])
-Koordinaten.append([cord2["lat"],cord2["lon"]])
+print(koordinaten)
 
 
-print(Koordinaten)
+#---Karte erstellen----
+
+mean_lat = sum(coord[0] for coord in koordinaten) / len(koordinaten)
+mean_lon = sum(coord[1] for coord in koordinaten) / len(koordinaten)
+
+m = folium.Map(
+    location=[mean_lat, mean_lon],
+    zoom_start=17,
+    tiles=xyz.Esri.WorldImagery
+)
+
+
+#---Marker setzen---
+for coordinate in koordinaten: #wenn es nur die coordinaten sein sollen
+    folium.Marker(
+        location=[coordinate[0], coordinate[1]],
+        popup="",
+        icon=folium.Icon(color="green")
+    ).add_to(m)
+
+m.save("map.html")
+
+
+
+
+
